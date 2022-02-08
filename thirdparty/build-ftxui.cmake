149d2d6afd0512c9375b270aa75f3a4ff9373a5a
Aleksandar Brakmić <13668697+brakmic-aleksandar@users.noreply.github.com>
2022-02-08 13:56:03 +0100

    Added ftxui dependancy

---
+++ thirdparty/build-ftxui.cmake
@@ -0,0 +1,8 @@
+option(FTXUI_BUILD_DOCS "Set to ON to build docs" OFF)
+option(FTXUI_BUILD_EXAMPLES "Set to ON to build examples" OFF)
+
+add_subdirectory(thirdparty/ftxui)
+
+#add_library(ftxui::screen ALIAS ftxui_screen)
+#add_library(ftxui::dom ALIAS ftxui_dom)
+#add_library(ftxui::component ALIAS ftxui_component)
